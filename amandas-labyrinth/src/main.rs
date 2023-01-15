2946e87f78bdbb0405d992bff5b6bb4f710adaab
KisicM <mkisic99@gmail.com>
2023-01-15 13:19:12 +0100

    find surrounding fields

--- amandas-labyrinth/src/main.rs
+++ amandas-labyrinth/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::fmt;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum Direction {
     W,
     E,
@@ -23,12 +23,14 @@ impl fmt::Display for Direction {
 }
 
 #[derive(Debug, Clone)]
+#[allow(dead_code)]
 struct AvailablePath {
     direction: Direction,
     has_door: bool
 }
 
 #[derive(Debug, Clone)]
+#[allow(dead_code)]
 struct Field {
     index: i32,
     has_key: bool,
@@ -43,13 +45,19 @@ impl Field {
 }
 
 fn main() {
+    let mut _steps: i32 = 0;
+    let mut _keys: i32 = 0;
+    let mut _exit: bool = false;
+    let mut labyrinth : Vec<Field> = vec![];
+    let mut exit_fields: Vec<Field> = vec![];
+    let mut current_field: Option<Field> = None;
+
     //File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./labyrinth.txt") {
         // Consumes the iterator, returns an (Optional) String
         let mut i: i32 = 0;
         for line in lines {
             if let Ok(ip) = line {
-                i += 1;
                 let mut binary_field = vec![];
                 for byte in ip.split_whitespace() {
                     binary_field.push(byte);
@@ -64,10 +72,22 @@ fn main() {
                     is_exit: has_key[2] == '1' && has_key[3] == '1',
                     available_paths: get_available_paths(binary_field[0], binary_field[1])
                 };
-                field.print();
+                //field.print();
+                if field.is_exit {
+                    exit_fields.push(field.clone());
+                };
+                labyrinth.push(field);
+                i += 1;
             }
         }
     }
+    //print!("{:#?}", labyrinth);
+    current_field = Some(labyrinth[0].clone());
+    //print!("{:#?}, vector len: {}\n", current_field, labyrinth.len());
+    let surrounding_fields = get_surrounding_fields(&labyrinth, current_field.unwrap());
+    println!("{:#?}", surrounding_fields);
+    
+
     
 }
 
@@ -96,4 +116,34 @@ fn get_available_paths(paths: &str, doors: &str) -> Box<[AvailablePath]> {
         i += 1;
     }
     return available_paths.into_boxed_slice();
+}
+
+fn get_surrounding_fields(labyrinth : &Vec<Field>, current_field: Field) -> Vec<&Field> {
+    let mut surrounding_fields: Vec<&Field> = vec![];
+    let available_paths = current_field.available_paths;
+    for path in available_paths.into_iter() {
+        let field_index: i32;
+        match  path.direction {
+            Direction::W => {
+                field_index = current_field.index - 1;
+            },
+            Direction::E => {
+                field_index = current_field.index + 1;
+            },
+            Direction::N => {
+                field_index = current_field.index - 9;
+            },
+            Direction::S => {
+                field_index = current_field.index + 9;
+            }
+        }
+        if field_index >= 0 && field_index <= 53 {
+            for field in labyrinth {
+                if field.index == field_index {
+                    surrounding_fields.push(field);
+                }
+            }
+        }
+    }
+    return surrounding_fields;
 }
